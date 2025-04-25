3ef9596bf332f1511242e0acc7db0a945576b770
CHHAVIDUBEY24 <chhavidubey2224@gmail.com>
2025-04-25 18:39:50 +0000

    vaiables

---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const userId=2428;
+let userName="Harsh";
+var userPass="ch2428";
+userCity="Indore";
+var userPhone;
+
+console.table([userId,userName,userPass,userPhone,userCity]);
+
+// prefer not to use var because of issues in block scope and functional scope
+// "use strict"->treat all js code as newer version
